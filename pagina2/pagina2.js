734f2cc1ffe54eee94024fc88fd33ee0648e68cc
Marcos Lozano <desarrollo@itsmarts.com.mx>
2024-09-13 16:09:31 -0600

    Actualización 13/Sep/2024
    Ejemplos simples con Js

--- pagina2/pagina2.js
+++ pagina2/pagina2.js
@@ -270,7 +270,60 @@ var texto = `
 	<h3>Mis apellidos son: ${apellidos}</h3>
 `;
 
-document.write(texto);  // OJO, imprime en el documento HTML y desaparece todo
+// document.write(texto);  // OJO, imprime en el documento HTML y desaparece todo
+this.alert(texto); // No funcionó con el alert.
+
+
+
+
+// Arrays, Arreglos, Matrices
+
+var nombre = "Victor Robles";
+var nombres = ["Victor Robles", "Juan Lopez", "Manolo Garcia", "Jose Martin", 52, true];
+var lenguajes = new Array("PHP", "JS", "Go", "Java", "C#", "C", "Pascal");
+
+var elemento = parseInt(prompt("Que elemento del array quieres??", 0));
+if(elemento >= nombres.length){
+	alert("Introduce el numero correcto menor que " + nombres.length);
+}else{
+	alert("El usuario seleccionado es: "+nombres[elemento]);
+}
+
+document.write("<h1>Lenguajes de programación del 2018</h1>");
+document.write("<ul>");
+
+
+
+// ESTA ES UNA FORMA DE RECORRER UN ARCHIVO: (Primer forma)
+for(var i = 0; i < lenguajes.length; i++){
+	document.write("<li> Primer For  "+lenguajes[i]+"</li>");
+}
+
+// ESTA ES UNA SEGUNDA FORMA DE RECORRER UN ARCHIVO: (Segunda forma)
+ lenguajes.forEach((elemento, indice)=>{
+ 	document.write("<li> Segundo For"+indice+" - "+elemento+"</li>");
+ });
+
+
+// Y ESTA ES UNA TERCER FORMA , para recorrer un Arreglo ( Tercer Forma , es la mas usada )
+for(let lenguaje in lenguajes){
+	document.write("<li> Tercer For  "+lenguajes[lenguaje]+"</li>");
+}
+document.write("</ul>");
+
+
+
+
+
+// Busquedas
+var precios = [10, 20, 50, 80, 12];
+
+//var busqueda = lenguajes.findIndex(lenguaje => lenguaje == "JS");
+var busqueda = precios.some(precio => precio < 10);
+
+console.log(busqueda);
+
+
 
 
 
